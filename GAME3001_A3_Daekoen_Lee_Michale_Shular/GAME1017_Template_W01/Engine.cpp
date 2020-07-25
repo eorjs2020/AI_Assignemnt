065bcb2489dac25860f619cb447352b971cfe8c6
david <eorjs2020@gmail.com>
2020-07-25 18:03:39 -0400

    Initial

---
+++ GAME3001_A3_Daekoen_Lee_Michale_Shular/GAME1017_Template_W01/Engine.cpp
@@ -0,0 +1,175 @@
+#include "Engine.h"
+#include "CollisionManager.h"
+#include "DebugManager.h"
+#include "EventManager.h"
+#include "FontManager.h"
+#include "SoundManager.h"
+#include "StateManager.h"
+#include "TextureManager.h"
+#include <iostream>
+#include <fstream>
+#define WIDTH 1024
+#define HEIGHT 768
+#define FPS 60
+using namespace std;
+
+Engine::Engine():m_running(false){ cout << "Engine class constructed!" << endl; }
+
+bool Engine::Init(const char* title, int xpos, int ypos, int width, int height, int flags)
+{
+	cout << "Initializing game..." << endl;
+	// Attempt to initialize SDL.
+	if (SDL_Init(SDL_INIT_EVERYTHING) == 0)
+	{
+		// Create the window.
+		m_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
+		if (m_pWindow != nullptr) // Window init success.
+		{
+			m_pRenderer = SDL_CreateRenderer(m_pWindow, -1, 0);
+			if (m_pRenderer != nullptr) // Renderer init success.
+			{
+				EVMA::Init();
+				SOMA::Init();
+				TEMA::Init();
+			}
+			else return false; // Renderer init fail.
+		}
+		else return false; // Window init fail.
+	}
+	else return false; // SDL init fail.
+	// Example specific initialization.
+	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "2"); // Call this before any textures are created.
+	m_pTileText = IMG_LoadTexture(m_pRenderer, "Img/Tiles.png");
+	m_pPlayerText = IMG_LoadTexture(m_pRenderer, "Img/Maga.png");
+	m_pPlayer = new Player({ 0,0,32,32 }, { (float)(width / 2 - 16), (float)(height / 2 - 16), 32, 32 }, m_pRenderer, m_pPlayerText, 0, 0, 0, 4);
+	ifstream inFile("Dat/Tiledata.txt");
+	if (inFile.is_open())
+	{ // Create map of Tile prototypes.
+		char key;
+		int x, y;
+		bool o, h;
+		while (!inFile.eof())
+		{
+			inFile >> key >> x >> y >> o >> h;
+			m_tiles.emplace(key, new Tile({ x * 32, y * 32, 32, 32 }, { 0,0,32,32 }, m_pRenderer, m_pTileText, o, h));
+		}
+	}
+	inFile.close();
+	inFile.open("Dat/Level1.txt");
+	if (inFile.is_open())
+	{ // Build the level from Tile prototypes.
+		char key;
+		for (int row = 0; row < ROWS; row++)
+		{
+			for (int col = 0; col < COLS; col++)
+			{
+				inFile >> key;
+				m_level[row][col] = m_tiles[key]->Clone(); // Prototype design pattern used.
+				m_level[row][col]->GetDstP()->x = (float)(32 * col);
+				m_level[row][col]->GetDstP()->y = (float)(32 * row);
+			}
+		}
+	}
+	inFile.close();
+	// Final engine initialization calls.
+	m_fps = (Uint32)round((1 / (double)FPS) * 1000); // Sets FPS in milliseconds and rounds.
+	m_running = true; // Everything is okay, start the engine.
+	cout << "Engine Init success!" << endl;
+	return true;
+}
+
+void Engine::Wake()
+{
+	m_start = SDL_GetTicks();
+}
+
+void Engine::Sleep()
+{
+	m_end = SDL_GetTicks();
+	m_delta = m_end - m_start;
+	if (m_delta < m_fps) // Engine has to sleep.
+		SDL_Delay(m_fps - m_delta);
+}
+
+void Engine::HandleEvents()
+{
+	EVMA::HandleEvents();
+}
+
+void Engine::Update()
+{
+	m_pPlayer->Update();
+}
+
+void Engine::Render() 
+{
+	SDL_SetRenderDrawColor(m_pRenderer, 0, 0, 0, 255);
+	SDL_RenderClear(m_pRenderer); // Clear the screen with the draw color.
+	// Draw anew.
+	for (int row = 0; row < ROWS; row++)
+	{
+		for (int col = 0; col < COLS; col++)
+		{
+			m_level[row][col]->Render();
+		}
+	}
+	m_pPlayer->Render();
+	// Flip buffers.
+	SDL_RenderPresent(m_pRenderer);
+}
+
+void Engine::Clean()
+{
+	cout << "Cleaning game." << endl;
+	// Example-specific cleaning.
+	for (int row = 0; row < ROWS; row++)
+	{
+		for (int col = 0; col < COLS; col++)
+		{
+			delete m_level[row][col];
+			m_level[row][col] = nullptr; // Wrangle your dangle.
+		}
+	}
+	for (auto const& i : m_tiles)
+		delete m_tiles[i.first];
+	m_tiles.clear();
+	// Finish cleaning.
+	SDL_DestroyRenderer(m_pRenderer);
+	SDL_DestroyWindow(m_pWindow);
+	DEMA::Quit();
+	EVMA::Quit();
+	FOMA::Quit();
+	SOMA::Quit();
+	STMA::Quit();
+	TEMA::Quit();
+	IMG_Quit();
+	SDL_Quit();
+}
+
+int Engine::Run()
+{
+	if (m_running) // What does this do and what can it prevent?
+		return -1; 
+	if (Init("GAME1017 Engine Template", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0) == false)
+		return 1;
+	while (m_running) // Main engine loop.
+	{
+		Wake();
+		HandleEvents();
+		Update();
+		Render();
+		if (m_running)
+			Sleep();
+	}
+	Clean();
+	return 0;
+}
+
+Engine& Engine::Instance()
+{
+	static Engine instance; // C++11 will prevent this line from running more than once. Magic statics.
+	return instance;
+}
+
+SDL_Renderer* Engine::GetRenderer() { return m_pRenderer; }
+bool& Engine::Running() { return m_running; }
